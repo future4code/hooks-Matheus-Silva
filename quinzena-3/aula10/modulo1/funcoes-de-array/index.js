077e92e8ebdd2e723241a2a1e32813d24b872338
nimitzufo <nimitzufo@protonmail.com>
2022-03-10 22:20:23 -0300

    another code review

--- quinzena-3/aula10/modulo1/funcoes-de-array/index.js
+++ quinzena-3/aula10/modulo1/funcoes-de-array/index.js
@@ -7,6 +7,7 @@
 // a) um array com todos os elementos do array original exceto o objeto cujo atributo 'apelido' é 'Chijo'
 
 // code 
+// 1.
 const pets = [
     { nome: "Lupin", raca: "Salsicha"},
     { nome: "Polly", raca: "Lhasa Apso"},
@@ -16,21 +17,60 @@ const pets = [
     { nome: "Caramelo", raca: "Vira-lata"},
 ]
 
-// a) Crie um novo array que contenha apenas o nome dos doguinhos
-
+// a) 
 const petsNome = pets.map(pets => {
     return pets.nome
 })
 
-// b) Crie um novo array apenas com os [cachorros salsicha]
+// b) 
 const petsSalsicha = pets.filter (pets => {
     return pets.raca === "Salsicha"
 })
 
-// c) Crie um novo array que possuirá mensagens para enviar para todos os clientes que são poodles. 
-// A mensagem deve ser: "Você ganhou um cupom de desconto de 10% para tosar o/a `[NOME]`!"
+// c)
 const petsPoodle = pets.filter (pets => {
     return pets.raca === "Poodle"
 }).map(petPoodle => {
     return `Você ganhou um cupom de desconto de 10% para tosar o/a ${petPoodle.nome}`;
+})
+
+// 2.
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+// a) 
+const produtosNome = produtos.map(produtos => {
+    return produtos.nome
+})
+
+// b) 
+const produtosDesconto = produtos.map(produtos => {
+    return {nome: produtos.nome,  preco: produtos.preco*0.95}
+})
+
+// c) 
+const produtosBebidas = produtos.filter(produtos => {
+    return produtos.categoria === "Bebidas"
+})
+
+// d) 
+const produtosYpe = produtos.filter(produtos => {
+    return produtos.nome.match("\.\*Ypê$")
+})
+
+// e) 
+const produtosFraseMarca = produtos.filter(produtos => {
+    return produtos.nome.match("\.\*Ypê$")
+}).map(produtoMarca => {
+    return `Compre ${produtoMarca.nome} por ${produtoMarca.preco}`
 })
